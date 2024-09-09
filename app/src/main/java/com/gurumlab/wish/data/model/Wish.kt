986d59f8befaf0dfcfdc6702819a90b463b6b91e
gurumdevv <gurumdevv@gmail.com>
2024-09-09 21:49:18 +0900

    chore: 소원(wish) data class 정의

---
+++ app/src/main/java/com/gurumlab/wish/data/model/Wish.kt
@@ -0,0 +1,19 @@
+package com.gurumlab.wish.data.model
+
+data class Wish(
+    val postId: Int,
+    val createdDate: String,
+    val startedDate: String,
+    val completedDate: String,
+    val posterId: String,
+    val developerId: String,
+    val posterName: String,
+    val developerName: String,
+    val title: String,
+    val status: Int,
+    val likes: Int,
+    val simpleDescription: String,
+    val detailDescription: List<DetailDescription>,
+    val features: List<String>,
+    val comment: String,
+)
