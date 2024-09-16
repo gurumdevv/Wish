cb692f3c03ba14f3c5d2ef639393b0ac0674dcf6
gurumdevv <gurumdevv@gmail.com>
2024-09-16 11:27:27 +0900

    feat: 응원해요 버튼 실패 케이스 구현

--- app/src/main/java/com/gurumlab/wish/ui/home/HomeScreen.kt
+++ app/src/main/java/com/gurumlab/wish/ui/home/HomeScreen.kt
@@ -1,15 +1,27 @@
 package com.gurumlab.wish.ui.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.gurumlab.wish.R
 import com.gurumlab.wish.ui.theme.backgroundColor
+import com.gurumlab.wish.ui.util.CustomSnackbarContent
 import com.gurumlab.wish.ui.util.CustomTopAppBar
 import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.launch
@@ -29,28 +41,42 @@ fun HomeContent(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel
 ) {
+    val context = LocalContext.current
     val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
     val wishes = viewModel.wishes.collectAsStateWithLifecycle()
     val pagerState = rememberPagerState(pageCount = { wishes.value.keys.size })
 
-    Column(modifier = modifier) {
-        CustomTopAppBar()
+    Box(modifier = modifier) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            CustomTopAppBar()
 
-        VerticalPager(state = pagerState) { page ->
-            val wishIdentifier = wishes.value.keys.elementAt(page)
-            val wishContent = wishes.value.values.elementAt(page)
-            WishCard(
-                wish = wishContent,
-                onStartClick = {},
-                onLikeClick = {
-                    scope.launch {
-                        viewModel.getLikes(wishIdentifier).single().let { currentCount ->
-                            if (currentCount == -1) //TODO("업데이트 실패 토스트 띄우기")
-                            else viewModel.updateLikeCount(wishIdentifier, currentCount + 1)
+            VerticalPager(state = pagerState) { page ->
+                val wishIdentifier = wishes.value.keys.elementAt(page)
+                val wishContent = wishes.value.values.elementAt(page)
+                WishCard(
+                    wish = wishContent,
+                    onStartClick = {},
+                    onLikeClick = {
+                        scope.launch {
+                            viewModel.getLikes(wishIdentifier).single().let { currentCount ->
+                                if (currentCount == -1) snackbarHostState.showSnackbar(
+                                    message = context.getString(R.string.fail_like_update),
+                                    duration = SnackbarDuration.Short
+                                )
+                                else viewModel.updateLikeCount(wishIdentifier, currentCount + 1)
+                            }
                         }
                     }
-                }
-            )
+                )
+            }
+        }
+
+        SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) { data ->
+            CustomSnackbarContent(data, Color.Red, Color.White, Icons.Outlined.Warning)
         }
     }
 }
--- app/src/main/java/com/gurumlab/wish/ui/util/CustomComposable.kt
+++ app/src/main/java/com/gurumlab/wish/ui/util/CustomComposable.kt
@@ -1,7 +1,9 @@
 package com.gurumlab.wish.ui.util
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -19,12 +22,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarData
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
@@ -101,7 +106,7 @@ fun CustomTopAppBar(modifier: Modifier = Modifier) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(start = 16.dp,  bottom = 5.dp)
+            .padding(start = 16.dp, bottom = 5.dp)
     ) {
         Image(
             modifier = Modifier.size(30.dp),
@@ -146,4 +151,41 @@ fun ScaffoldTopAppBar(
             }
         }
     )
+}
+
+@Composable
+fun CustomSnackbarContent(
+    snackbarData: SnackbarData,
+    backgroundColor: Color,
+    textColor: Color,
+    icon: ImageVector? = null
+) {
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clip(RoundedCornerShape(10.dp))
+            .background(backgroundColor)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = stringResource(R.string.snackbar_icon),
+                    tint = textColor,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+            Text(
+                text = snackbarData.visuals.message,
+                color = textColor,
+                fontSize = 14.sp
+            )
+        }
+    }
 }
