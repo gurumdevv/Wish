1b2357138f054550353af5f1fc5373fe7d00bc49
gurumdevv <gurumdevv@gmail.com>
2024-10-07 17:12:00 +0900

    chore: 서비스 이용 정책 URL 수정

--- app/src/main/java/com/gurumlab/wish/ui/util/URL.kt
+++ app/src/main/java/com/gurumlab/wish/ui/util/URL.kt
@@ -2,9 +2,9 @@ package com.gurumlab.wish.ui.util
 
 object URL {
     const val TERMS_AND_CONDITION =
-        "https://firebase.google.com/terms/" //TODO("서비스 이용 약관 작성후 URL 변경하기")
+        "https://sites.google.com/view/wish-terms-and-condition"
     const val PRIVACY_POLICY =
-        "https://firebase.google.com/support/privacy/" //TODO("개인정보처리방침 작성후 URL 변경하기")
+        "https://sites.google.com/view/wish-privacy"
     const val DEFAULT_WISH_PHOTO =
         "https://firebasestorage.googleapis.com/v0/b/wish-a3649.appspot.com/o/default_image%2Fimage_wish_default.png?alt=media&token=8541851c-ed22-44f0-8ccb-e231cb23d86e"
 }
