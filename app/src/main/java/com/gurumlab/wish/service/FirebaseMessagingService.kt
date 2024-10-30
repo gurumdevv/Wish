379c68e6c95b3560bf59a5cf62a3fb32da96e8fc
gurumdevv <gurumdevv@gmail.com>
2024-10-30 23:55:04 +0900

    feat: FCM을 위한 설정

---
+++ app/src/main/java/com/gurumlab/wish/service/FirebaseMessagingService.kt
@@ -0,0 +1,11 @@
+package com.gurumlab.wish.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FirebaseMessagingService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
