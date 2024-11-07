e4c06f1f72eec87f1026520ad353ac98f746a636
gurumdevv <gurumdevv@gmail.com>
2024-11-07 23:41:00 +0900

    chore: 화면 회전을 세로로 제한

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         applicationId = "com.gurumlab.wish"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
