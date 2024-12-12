2a64d28ce5b4451672b3b15134c8b40f6d2d1dc5
gurumdevv <gurumdevv@gmail.com>
2024-12-12 17:02:47 +0900

    feat: 상세 화면 진입시 자연스럽게 상세 내용이 표시 되도록, 화면 상단까지 올라온 후 상세 내용이 보이도록 수정

--- app/src/main/java/com/gurumlab/wish/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/gurumlab/wish/ui/detail/DetailScreen.kt
@@ -1,5 +1,9 @@
 package com.gurumlab.wish.ui.detail
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Ease
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +14,9 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,6 +27,7 @@ import com.gurumlab.wish.data.model.MinimizedWish
 import com.gurumlab.wish.ui.theme.backgroundColor
 import com.gurumlab.wish.ui.util.ErrorSnackBar
 import com.gurumlab.wish.ui.util.showSnackbar
+import kotlinx.coroutines.delay
 
 @Composable
 fun DetailScreen(
@@ -65,71 +72,83 @@ fun DetailContent(
     val snackbarHostState = remember { SnackbarHostState() }
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val startingWishUiState by viewModel.startingWishUiState.collectAsStateWithLifecycle()
+    var isShowList by remember { mutableStateOf(false) }
 
-    when (uiState) {
-        DetailUiState.Loading -> {
-            DetailLoadingScreen(modifier)
-        }
+    LaunchedEffect(Unit) {
+        delay(180)
+        isShowList = true
+    }
 
-        DetailUiState.Fail -> {
-            DetailFailScreen(wishId = wishId, viewModel = viewModel)
-        }
-
-        is DetailUiState.Success -> {
-            val wish = (uiState as DetailUiState.Success).wish
-            val currentDate = remember { viewModel.fetchCurrentDate() }
-            val currentUser = remember { viewModel.fetchUserInfo() }
-            val minimizedWish =
-                remember {
-                    viewModel.fetchMinimizedWish(
-                        wish = wish,
-                        currentDate = currentDate,
-                        currentUser = currentUser
-                    )
-                }
-
-            Box(
-                modifier = modifier
-            ) {
-                ProjectDescriptionArea(wish)
-
-                if (wish.posterId != currentUser.uid) {
-                    DetailScreenButtonArea(
-                        minimizedWish = minimizedWish,
-                        onUpdateWish = {
-                            viewModel.updateWish(
-                                wishId = wishId,
-                                currentDate = currentDate,
-                                currentUser = currentUser,
-                                alreadyBegunMessageRes = R.string.already_begun_message,
-                                failSnackbarMessageRes = R.string.please_try_again_later
-                            )
-                        },
-                        onMessageScreen = onMessageScreen,
-                        modifier = Modifier.align(Alignment.BottomCenter)
-                    )
-                }
-
-                ErrorSnackBar(
-                    snackbarHostState = snackbarHostState,
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .offset(y = (-102).dp)
-                )
+    AnimatedVisibility(
+        modifier = Modifier.fillMaxSize(),
+        visible = isShowList,
+        enter = fadeIn(animationSpec = tween(durationMillis = 100, easing = Ease))
+    ) {
+        when (uiState) {
+            DetailUiState.Loading -> {
+                DetailLoadingScreen(modifier)
             }
 
-            LaunchedEffect(
-                startingWishUiState.isStatusUpdateSuccess,
-                startingWishUiState.isStartedDateUpdateSuccess,
-                startingWishUiState.isDeveloperIdUpdateSuccess,
-                startingWishUiState.isDeveloperNameUpdateSuccess
-            ) {
-                if (startingWishUiState.isStatusUpdateSuccess &&
-                    startingWishUiState.isStartedDateUpdateSuccess &&
-                    startingWishUiState.isDeveloperIdUpdateSuccess &&
+            DetailUiState.Fail -> {
+                DetailFailScreen(wishId = wishId, viewModel = viewModel)
+            }
+
+            is DetailUiState.Success -> {
+                val wish = (uiState as DetailUiState.Success).wish
+                val currentDate = remember { viewModel.fetchCurrentDate() }
+                val currentUser = remember { viewModel.fetchUserInfo() }
+                val minimizedWish =
+                    remember {
+                        viewModel.fetchMinimizedWish(
+                            wish = wish,
+                            currentDate = currentDate,
+                            currentUser = currentUser
+                        )
+                    }
+
+                Box(
+                    modifier = modifier
+                ) {
+                    ProjectDescriptionArea(wish)
+
+                    if (wish.posterId != currentUser.uid) {
+                        DetailScreenButtonArea(
+                            minimizedWish = minimizedWish,
+                            onUpdateWish = {
+                                viewModel.updateWish(
+                                    wishId = wishId,
+                                    currentDate = currentDate,
+                                    currentUser = currentUser,
+                                    alreadyBegunMessageRes = R.string.already_begun_message,
+                                    failSnackbarMessageRes = R.string.please_try_again_later
+                                )
+                            },
+                            onMessageScreen = onMessageScreen,
+                            modifier = Modifier.align(Alignment.BottomCenter)
+                        )
+                    }
+
+                    ErrorSnackBar(
+                        snackbarHostState = snackbarHostState,
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .offset(y = (-102).dp)
+                    )
+                }
+
+                LaunchedEffect(
+                    startingWishUiState.isStatusUpdateSuccess,
+                    startingWishUiState.isStartedDateUpdateSuccess,
+                    startingWishUiState.isDeveloperIdUpdateSuccess,
                     startingWishUiState.isDeveloperNameUpdateSuccess
                 ) {
-                    onProgressScreen(minimizedWish, wishId)
+                    if (startingWishUiState.isStatusUpdateSuccess &&
+                        startingWishUiState.isStartedDateUpdateSuccess &&
+                        startingWishUiState.isDeveloperIdUpdateSuccess &&
+                        startingWishUiState.isDeveloperNameUpdateSuccess
+                    ) {
+                        onProgressScreen(minimizedWish, wishId)
+                    }
                 }
             }
         }
