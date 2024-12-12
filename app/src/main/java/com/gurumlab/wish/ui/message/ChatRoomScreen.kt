90d9fbf4f31ccef0b956cefcf63e4c5a6c45b885
gurumdevv <gurumdevv@gmail.com>
2024-12-12 16:55:35 +0900

    feat: 채팅 화면 진입시 자연스럽게 채팅 리스트가 표시 될 수 있도록, 화면 상단까지 올라온 후 채팅 리스트가 보이도록 수정

--- app/src/main/java/com/gurumlab/wish/ui/message/ChatRoomScreen.kt
+++ app/src/main/java/com/gurumlab/wish/ui/message/ChatRoomScreen.kt
@@ -1,5 +1,9 @@
 package com.gurumlab.wish.ui.message
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Ease
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +20,9 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -28,6 +34,7 @@ import com.gurumlab.wish.data.model.ChatRoom
 import com.gurumlab.wish.ui.theme.backgroundColor
 import com.gurumlab.wish.ui.util.ErrorSnackBar
 import com.gurumlab.wish.ui.util.showSnackbar
+import kotlinx.coroutines.delay
 
 @Composable
 fun ChatRoomScreen(
@@ -84,53 +91,64 @@ fun ChatRoomContent(
     val screenWidth = remember { configuration.screenWidthDp.dp }
     val snackbarHostState = remember { SnackbarHostState() }
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
     val listState = rememberLazyListState()
+    var isShowList by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        delay(180)
+        isShowList = true
+    }
 
     Box(modifier = modifier) {
-        Column(
-            modifier = Modifier.fillMaxSize()
+        AnimatedVisibility(
+            modifier = Modifier.fillMaxSize(),
+            visible = isShowList,
+            enter = fadeIn(animationSpec = tween(durationMillis = 100, easing = Ease))
         ) {
-            when (uiState) {
-                ChatRoomUiState.Loading -> {
-                    ChatRoomLoadingScreen(modifier = Modifier.weight(1f))
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                when (uiState) {
+                    ChatRoomUiState.Loading -> {
+                        ChatRoomLoadingScreen(modifier = Modifier.weight(1f))
+                    }
+
+                    ChatRoomUiState.Fail -> {
+                        ChatFailScreen(modifier = Modifier.weight(1f))
+                    }
+
+                    is ChatRoomUiState.Success -> {
+                        val messages = (uiState as ChatRoomUiState.Success).messages
+                        ChatList(
+                            state = listState,
+                            chatList = messages,
+                            currentUserUid = viewModel.getUid(),
+                            othersUserName = othersUserName,
+                            othersUserImageUrl = othersUserImageUrl,
+                            screenWidth = screenWidth,
+                            isReverse = true,
+                            onRepository = onRepository,
+                            onDonation = onDonation,
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
                 }
 
-                ChatRoomUiState.Fail -> {
-                    ChatFailScreen(modifier = Modifier.weight(1f))
-                }
-
-                is ChatRoomUiState.Success -> {
-                    val messages = (uiState as ChatRoomUiState.Success).messages
-                    ChatList(
-                        state = listState,
-                        chatList = messages,
-                        currentUserUid = viewModel.getUid(),
-                        othersUserName = othersUserName,
-                        othersUserImageUrl = othersUserImageUrl,
-                        screenWidth = screenWidth,
-                        isReverse = true,
-                        onRepository = onRepository,
-                        onDonation = onDonation,
-                        modifier = Modifier.weight(1f)
-                    )
-                }
+                ChatInput(
+                    text = viewModel.messageUiState.message,
+                    isEnabled = viewModel.messageUiState.isChatEnabled,
+                    onTextChange = { viewModel.updateMessage(it) },
+                    onSendClick = {
+                        viewModel.sendMessage(
+                            othersFcmToken = othersFcmToken,
+                            defaultTitle = context.getString(R.string.default_push_message_title)
+                        )
+                    },
+                    modifier = Modifier
+                        .consumeWindowInsets(WindowInsets.navigationBars)
+                        .imePadding()
+                )
             }
-
-            ChatInput(
-                text = viewModel.messageUiState.message,
-                isEnabled = viewModel.messageUiState.isChatEnabled,
-                onTextChange = { viewModel.updateMessage(it) },
-                onSendClick = {
-                    viewModel.sendMessage(
-                        othersFcmToken = othersFcmToken,
-                        defaultTitle = context.getString(R.string.default_push_message_title)
-                    )
-                },
-                modifier = Modifier
-                    .consumeWindowInsets(WindowInsets.navigationBars)
-                    .imePadding()
-            )
         }
 
         ErrorSnackBar(
